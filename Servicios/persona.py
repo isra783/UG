2724eff3182a25552cf83ead7d4705aaebae064e
User <ug-fca-gig@ug.edu.ec>
2026-01-09 08:55:25 -0500

    ...

---
+++ Servicios/persona.py
@@ -0,0 +1,19 @@
+from PySide6.QtWidgets import QMainWindow
+
+
+class PersonaServicio(QMainWindow):
+    '''
+    class que genera la logica de los objetos personas
+    '''
+    def __init__(self):
+        super(PersonaServicio, self).__init__()
+
+
+
+
+
+
+
+
+
+
